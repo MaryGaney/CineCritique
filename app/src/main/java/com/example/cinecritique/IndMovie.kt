743a4642227299ff7e2b60bf648795991d9ed982
shiva <siyanar@iu.edu>
2024-12-15 14:52:57 -0500

    made pfp image in each page take you to user page. need to add logout

--- app/src/main/java/com/example/cinecritique/IndMovie.kt
+++ app/src/main/java/com/example/cinecritique/IndMovie.kt
@@ -1,12 +1,20 @@
 package com.example.cinecritique
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class IndMovie : AppCompatActivity() {
+
+    companion object {
+        var userId: String? = null
+        private const val ADD_TASK_REQUEST_CODE = 1
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +23,14 @@ class IndMovie : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        //profile pic image takes you to user page
+        val pfp: ImageView = findViewById(R.id.pfp)
+        //val pf: ImageView = findViewById(R.id.imageView)
+        pfp.setOnClickListener {
+            val intent = Intent(this, UserPage::class.java)
+            startActivityForResult(intent, ADD_TASK_REQUEST_CODE)
         }
     }
 }
