579c3baa78aea38c0b25f36e9767401100e51ed8
shiva <siyanar@iu.edu>
2024-12-15 14:24:01 -0500

    added login and made pfp image in main page take you to user page. need to add logout and image click onto other pages

--- app/src/main/java/com/example/cinecritique/MainActivity.kt
+++ app/src/main/java/com/example/cinecritique/MainActivity.kt
@@ -1,15 +1,23 @@
 package com.example.cinecritique
 
+import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
 import com.google.android.material.button.MaterialButton
+import com.google.firebase.auth.FirebaseAuth
 import com.google.gson.GsonBuilder
 import com.google.gson.Strictness
 import okhttp3.OkHttpClient
@@ -21,6 +29,18 @@ import java.util.concurrent.Executors
 
 
 class MainActivity : AppCompatActivity() {
+
+    private val signInLauncher = registerForActivityResult(
+        FirebaseAuthUIActivityResultContract(),
+    ) { res ->
+        this.onSignInResult(res)
+    }
+
+    companion object {
+        var userId: String? = null
+        private const val ADD_TASK_REQUEST_CODE = 1
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -29,6 +49,26 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        //sign in stuff
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+//            AuthUI.IdpConfig.PhoneBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build()
+//            AuthUI.IdpConfig.FacebookBuilder().build(),
+//            AuthUI.IdpConfig.TwitterBuilder().build()
+        )
+
+        val signInIntent = AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(providers).build()
+        signInLauncher.launch(signInIntent)
+
+        //profile pic image takes you to user page
+        val pfp: ImageView = findViewById(R.id.pfp)
+        //val pf: ImageView = findViewById(R.id.imageView)
+        pfp.setOnClickListener {
+            val intent = Intent(this, UserPage::class.java)
+            startActivityForResult(intent, ADD_TASK_REQUEST_CODE)
         }
 
 
@@ -69,6 +109,21 @@ class MainActivity : AppCompatActivity() {
         myList.setLayoutManager(layoutManager);
          */
     }
+
+    private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult){
+        val response = result.idpResponse
+        if(result.resultCode == RESULT_OK){
+            //successful sign in
+            val user = FirebaseAuth.getInstance().currentUser
+            user?.let{
+                //do something here
+                Log.i("MYTAG","signed in")
+            }
+        } else{
+            Log.i("MYTAG","failed sign in")
+        }
+    }
+
     private fun showRatingBar(
         mButton1: MaterialButton,
         mButton2: MaterialButton,
